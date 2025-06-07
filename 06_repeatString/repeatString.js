b0f6871a57e1ec1b53269b0628cd682bef66fb28
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-07 19:40:20 +0800

    Finish join strings exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let string = ""
+    if (count < 0) {
+        return "ERROR";
+    } else {
+    for (let i = 1; i <= count; i++) {
+        string+=str
+    }
+    }
+    return string;
 };
 
 // Do not edit below this line
