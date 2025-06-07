7e97dda5ced16ed63f4e5bfcdde2d09d757ed0fd
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-07 19:55:13 +0800

    Finish 07, allowing me to reverse the string.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    arr = string.split("");
+    return arr.reverse().join("");
 };
+
+//takes input string
+//split string into array
+//for let letter = array.length; letter >= 0; letter--
+//pop array[letter]
+//unshift array[letter]
 
 // Do not edit below this line
 module.exports = reverseString;
