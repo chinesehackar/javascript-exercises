877d86b37576e1d551876ebac79e4be7a631d5bf
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-08 11:18:22 +0800

    Finish 11 - tempConversion
    realized that toFixed() returns string, so need to reconvert it back
    using Number or parseFloat.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheight) {
+  let result =  (fahrenheight-32)*(5/9)
+  return Math.floor(result) === result ? result
+    : parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = ((celsius*9/5)+32)
+  return Math.floor(result) === result ? result
+    : parseFloat(result.toFixed(1));
 };
+
+//toFixed(1) = one decimal place but returns string
+//parseFLoat = converts string to float.
 
 // Do not edit below this line
 module.exports = {
