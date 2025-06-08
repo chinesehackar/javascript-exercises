0c19f5b605510bf6ddc6954a59e63c75ff51d06c
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-08 10:50:49 +0800

    Finish 10 leapYears.
    important to simulate test cases inside your function.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//configurations:
+//divisible by 4 => true
+//divisible by 100 but not 400 ==> false
+//divisible by 400 => true
 
 // Do not edit below this line
 module.exports = leapYears;
