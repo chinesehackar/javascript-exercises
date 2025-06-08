d3f979445e39aa2bd5e4f1e90f369cdda36d7dd9
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-08 09:11:53 +0800

    Finish 08 - removeFromArray.
    hardest exercise so far. Please don't just remember the methods, let it be a part of you.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...myArgs) {
+    /*let newArr = [];
+    array.forEach((item) => {
+        if (!myArgs.includes(item)) {
+            newArr.push(item);
+        }
+    })
+    return newArr;
+    */
+    return array.filter(item => !myArgs.includes(item))
 };
+
+//init function that takes array and indefinite param
+//init new array
+//forEach item in myArgs, check if array includes that item. 
+//If else, then push to new Array.
 
 // Do not edit below this line
 module.exports = removeFromArray;
