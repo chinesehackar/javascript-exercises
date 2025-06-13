4650f3f03770789408b186ee3002212f78b9cd2a
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-14 01:45:58 +0800

    Finish 12-14 js exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let baseString = str.replace(/ |, /g, "").split("").join("");
+    if (baseString[baseString.length-1] === '.' || baseString[baseString.length-1] === '!') {
+        baseString = baseString.slice(0, baseString.length-1).toLowerCase();
+    }
 
+    let reversedString = baseString.split('').reverse().join('')
+    if (baseString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    let sequence = [];
+    if (number === 0 || number === 1) {
+        sequence.push(number);
+    } else if (number < 0) {
+        return "OOPS";
+    } else if (number > 1) {
+        sequence.push(1, 1);
+        for (let i = 1; i < number-1; i++) {
+            let sumNumber = sequence[i] + sequence[i-1];
+            sequence.push(sumNumber);
+        }
+    }
+    return sequence[sequence.length-1]
 };
 
 // Do not edit below this line
