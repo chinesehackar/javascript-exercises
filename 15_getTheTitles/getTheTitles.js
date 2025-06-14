3e9cc0b955d5f09ebda16038ded94dd59a781889
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-14 20:16:15 +0800

    Finish 15,16 js exercises

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+getTheTitles = function(books) {
+    bookTitles = [];
+    books.forEach(book => {
+        bookTitles.push(book.title);
+    }) 
+    return bookTitles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const mapped = people.map(person => {
+        if (person.yearOfDeath == undefined) {
+            const d = new Date()
+            const year = d.getFullYear();
+            person.yearOfDeath = year;
+        }
+        return person;
+    })
+    const sortAge = mapped.sort((a , b) => {
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    })
+    return sortAge[0];
 };
+
+//for person of people, take person.death - person.birth
+
+//if yearofdeath then year of death - year of birth. return person object
 
 // Do not edit below this line
 module.exports = findTheOldest;
